3fa0b8a33eabcf2042edb32301754079278e074b
Fernando <fernando_aluani@hotmail.com>
2013-05-30 15:51:10 -0300

    Removi MapActivity do gps, coloquei o mapa na main mesmo. Ele agora inicializa centralizado na USP. Copiei os dados da tabela dos circulares para um xml no projeto;
    coloquei minha chave debug no repositorio

--- gps/eclipse-project/src/fefzjon/ep2/gps/MainActivity.java
+++ gps/eclipse-project/src/fefzjon/ep2/gps/MainActivity.java
@@ -1,25 +1,37 @@
 package fefzjon.ep2.gps;
 
-import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 import android.view.Menu;
 
-public class MainActivity extends Activity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+
+public class MainActivity extends FragmentActivity {
+
+	private GoogleMap map;
 
 	@Override
 	protected void onCreate(final Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		this.setContentView(R.layout.activity_main);
 
+		this.map = ((SupportMapFragment) this.getSupportFragmentManager()
+				.findFragmentById(R.id.map)).getMap();
+		this.map.setMyLocationEnabled(true);
+		this.map.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+		LatLng center = new LatLng(-23.561706, -46.725719);
+		this.map.moveCamera(CameraUpdateFactory.newLatLngZoom(center, 13.5f));
 	}
 
 	@Override
 	public void onStart() {
 		super.onStart();
-		Intent intent = new Intent(this, MapActivity.class);
+		// Intent intent = new Intent(this, MapActivity.class);
 		// intent.putExtra(EXTRA_MESSAGE, message);
-		this.startActivity(intent);
+		// this.startActivity(intent);
 	}
 
 	@Override
