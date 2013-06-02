b9ffa1540a58165abf1346b56f27bd7f8f462066
Fernando <fernando_aluani@hotmail.com>
2013-06-02 18:16:10 -0300

    Implementando refresh no ComentariosActivity

--- bandex/eclipse-project/src/fefzjon/ep2/bandejao/ComentariosActivity.java
+++ bandex/eclipse-project/src/fefzjon/ep2/bandejao/ComentariosActivity.java
@@ -5,6 +5,8 @@ import java.util.List;
 import android.app.ListActivity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ListAdapter;
@@ -16,8 +18,8 @@ import fefzjon.ep2.bandejao.utils.IntentKeys;
 
 public class ComentariosActivity extends ListActivity {
 
-	private int	bandexId;
-	private int	mealId;
+	private int bandexId;
+	private int mealId;
 
 	@Override
 	protected void onCreate(final Bundle savedInstanceState) {
@@ -33,21 +35,45 @@ public class ComentariosActivity extends ListActivity {
 		this.bandexId = intent.getIntExtra(IntentKeys.DETAILS_BANDECO_ID, 1);
 		this.mealId = intent.getIntExtra(IntentKeys.DETAILS_MEAL_ID, 1);
 
-		List<BandexComment> commentList = ComentariosManager.fetchAndParseComment(this.mealId);
-
-		ListAdapter adapter = new ComentariosAdapter(this, commentList);
-
-		this.setListAdapter(adapter);
+		this.doRefresh();
 
 		Button btPostar = (Button) this.findViewById(R.id.bt_postar_comentario);
 		btPostar.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(final View v) {
-				Intent intent = new Intent(ComentariosActivity.this, PostarActivity.class);
-				intent.putExtra(IntentKeys.DETAILS_MEAL_ID, ComentariosActivity.this.mealId);
+				Intent intent = new Intent(ComentariosActivity.this,
+						PostarActivity.class);
+				intent.putExtra(IntentKeys.DETAILS_MEAL_ID,
+						ComentariosActivity.this.mealId);
 				ComentariosActivity.this.startActivity(intent);
 			}
 		});
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(final Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		this.getMenuInflater().inflate(R.menu.comentarios, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(final MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.refresh_comments:
+			this.doRefresh();
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+	public void doRefresh() {
+		List<BandexComment> commentList = ComentariosManager
+				.fetchAndParseComment(this.mealId);
+
+		ListAdapter adapter = new ComentariosAdapter(this, commentList);
+
+		this.setListAdapter(adapter);
 	}
 
 	private void tryLogin() {
